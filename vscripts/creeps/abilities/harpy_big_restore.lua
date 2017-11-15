45dcf9166316769541e775e9d46a437670b403f1
Sheodar <kkabachok@mail.ru>
2017-11-15 18:57:18 +0200

    add harpy_big_restore

---
+++ vscripts/creeps/abilities/harpy_big_restore.lua
@@ -0,0 +1,16 @@
+harpy_big_restore = class({})
+
+--------------------------------------------------------------------------------
+
+function harpy_big_restore:OnSpellStart()
+	self.mana_restore = self:GetSpecialValueFor( "mana_restore" )
+    local target = self:GetCursorTarget()
+    target:GiveMana( self.mana_restore)
+    for i = 0, 2 do
+        local particle = ParticleManager:CreateParticle("particles/econ/items/templar_assassin/templar_assassin_butterfly/templar_assassin_trap_explode_beam_butterfly.vpcf", PATTACH_CUSTOMORIGIN, nil )
+        ParticleManager:SetParticleControl( particle, 0, target:GetOrigin() )
+--        ParticleManager:SetParticleControl( particle, 1, target:GetOrigin() )
+    end
+end
+
+--------------------------------------------------------------------------------
